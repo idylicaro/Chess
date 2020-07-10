4625075dd0ea62cfc8ef76f168d698995f896aac
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-07-10 15:31:56 -0300

    feat: load prefabs pieces and create Spawn Method

--- Chess/Assets/Scripts/BoardManager.cs
+++ Chess/Assets/Scripts/BoardManager.cs
@@ -13,6 +13,13 @@ public class BoardManager : MonoBehaviour
     public List<GameObject> chessmanPrefabs;
     private List<GameObject> activeChessman;
 
+    private Quaternion orientation = Quaternion.Euler(0, 180, 0);
+
+    private void Start()
+    {
+        SpawnAllChessmans();
+    }
+
     private void Update()
     {
         UpdateSelection();
@@ -35,6 +42,79 @@ public class BoardManager : MonoBehaviour
             selectionY = -1;
         }
     }
+
+    private void SpawnChessman(int index, Vector3 position)
+    {
+        GameObject go = Instantiate(chessmanPrefabs[index], position, orientation) as GameObject;
+        go.transform.SetParent(transform);
+        activeChessman.Add(go);
+    }
+    private void SpawnChessman(int index, Vector3 position, Quaternion orientation)
+    {
+        GameObject go = Instantiate(chessmanPrefabs[index], position, orientation) as GameObject;
+        go.transform.SetParent(transform);
+        activeChessman.Add(go);
+    }
+
+    private void SpawnAllChessmans()
+    {
+        Quaternion orientationWhite = Quaternion.Euler(0, 0, 0);
+        activeChessman = new List<GameObject>();
+
+        //Spawn white team 
+
+        // King 
+        SpawnChessman(0, GetTileCenter(3,0));
+        
+        // Queen 
+        SpawnChessman(1, GetTileCenter(4,0));
+
+        // Rooks 
+        SpawnChessman(2, GetTileCenter(0, 0));
+        SpawnChessman(2, GetTileCenter(7, 0));
+
+        // Bishops 
+        SpawnChessman(3, GetTileCenter(2, 0));
+        SpawnChessman(3, GetTileCenter(5, 0));
+
+        // Knigths 
+        SpawnChessman(4, GetTileCenter(1, 0), orientationWhite);
+        SpawnChessman(4, GetTileCenter(6, 0), orientationWhite);
+
+        // Pawns 
+        for(int i = 0; i < 8; i++) SpawnChessman(5, GetTileCenter(i, 1));
+
+        //Spawn black team 
+
+        // King 
+        SpawnChessman(6, GetTileCenter(3, 7));
+
+        // Queen 
+        SpawnChessman(7, GetTileCenter(4, 7));
+
+        // Rooks 
+        SpawnChessman(8, GetTileCenter(0, 7));
+        SpawnChessman(8, GetTileCenter(7, 7));
+
+        // Bishops 
+        SpawnChessman(9, GetTileCenter(2, 7));
+        SpawnChessman(9, GetTileCenter(5, 7));
+
+        // Knigths 
+        SpawnChessman(10, GetTileCenter(1, 7));
+        SpawnChessman(10, GetTileCenter(6, 7));
+
+        // Pawns 
+        for (int i = 0; i < 8; i++) SpawnChessman(11, GetTileCenter(i, 6));
+    }
+    private Vector3 GetTileCenter(int x , int y)
+    {
+        Vector3 origin = Vector3.zero;
+        origin.x += (TILE_SIZE * x) + TILE_OFFSET;
+        origin.z += (TILE_SIZE * y) + TILE_OFFSET;
+        return origin;
+    }
+
     private void DrawChessboard()
     {
         Vector3 widthLine = Vector3.right * 8;
